818f092b33117f1240432e03373b30b8f8fd75b4
Ana Luisa Silva do Amaral <contato.analusilvamarau@gmail.com>
2025-09-28 17:14:53 -0300

    Ajuste nome variável e adição de adição de services

---
+++ currency-service/src/main/java/br/edu/atitus/currency_services/CurrencyServiceApplication.java
@@ -0,0 +1,13 @@
+package br.edu.atitus.currency_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyServiceApplication.class, args);
+	}
+
+}
---
+++ greeting-service/src/main/java/br/edu/atitus/greeting_services/dtos/GreetingDTO.java
@@ -0,0 +1,5 @@
+package br.edu.atitus.greeting_services.dtos;
+
+public record GreetingDTO(String name) {
+
+}
